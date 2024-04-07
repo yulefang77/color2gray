330397925ef521a5a3db94633e4ba2ba0ede6e8b
yule <yulefang77@gmail.com>
2024-04-07 17:34:39 +0800

    tranform colorful pictures grayscale

---
+++ color2gray.py
@@ -0,0 +1,9 @@
+import os
+from PIL import Image
+
+for file in os.listdir('origin'):
+	if file.endswith('.jpg'):
+		img = Image.open('origin/' + file) #file path
+		img = img.convert('L') # convert to L (Grayscale)
+		img.save('result/' + file[:-4] + '_gray.jpg') #file path
+		img.close()
