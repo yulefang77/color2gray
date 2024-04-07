dd492dc1cbcfebab99a267df3a1193e2157a4dd6
yule <yulefang77@gmail.com>
2024-04-07 17:37:26 +0800

    use os.path.join()

--- color2gray.py
+++ color2gray.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 for file in os.listdir('origin'):
 	if file.endswith('.jpg'):
-		img = Image.open('origin/' + file) #file path
+		img = Image.open(os.path.join('origin', file)) #file path
 		img = img.convert('L') # convert to L (Grayscale)
-		img.save('result/' + file[:-4] + '_gray.jpg') #file path
+		img.save(os.path.join('result', file[:-4] + '_gray.jpg')) #file path
 		img.close()
